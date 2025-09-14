0bafac0659d1f3802b9cc5dcd64bfb731eddba11
Mayank Agrawal <mayankagrawal@Mayanks-MacBook-Air.local>
2025-09-14 18:12:05 +0530

    changing the backend url

--- frontend/app/(dashboard)/page.tsx
+++ frontend/app/(dashboard)/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             return
         }
 
-        const socket = new WebSocket("wss://paychat-2.onrender.com")
+        const socket = new WebSocket("wss://paychatbackend.alignstacks.com")
 
         socket.onopen = () => {
             console.log("connected")
